438d1e0aa3200a6ed1a21eac2356f2f793de02af
Alark <67312907+nbcctwya@users.noreply.github.com>
2025-01-25 21:50:26 +0800

    Create 09softmax-regression-concise-implement.py

---
+++ Chapter3-Linear-neural-network/09softmax-regression-concise-implement.py
@@ -0,0 +1,24 @@
+# softmax回归的简洁实现
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size=batch_size)
+
+
+net = nn.Sequential(nn.Flatten(), nn.Linear(784, 10))
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, std=0.01)
+
+net.apply(init_weights)
+# 交叉熵损失函数中传递未归一化的预测，并同时计算softmax及其对数
+loss = nn.CrossEntropyLoss()
+
+trainer = torch.optim.SGD(net.parameters(), lr=0.1)
+
+num_epochs = 10
+d2l.train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
+d2l.plt.show()
